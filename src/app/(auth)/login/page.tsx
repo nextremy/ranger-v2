a2cf8ecac19665f2fd1a50453cba6f23d3cc5baf
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-26 13:52:35 -0700

    Implement login

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { Button } from "@/components/button";
+import { Form } from "@/components/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FormEventHandler, useId } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(1, "Username is required"),
+  password: z.string().min(1, "Password is required"),
+});
+type FormSchema = z.infer<typeof formSchema>;
+
+export default function Login() {
+  const usernameInputId = useId();
+  const passwordInputId = useId();
+  const form = useForm<FormSchema>({ resolver: zodResolver(formSchema) });
+  const router = useRouter();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    form.handleSubmit(async (data) => {
+      const response = await fetch("/api/users/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        if (response.status === 401) {
+          form.setError("root", {
+            message: "Invalid username or password",
+          });
+          return;
+        }
+        form.setError("root", { message: "Unknown server error" });
+        return;
+      }
+      router.replace("/");
+    })(event);
+  };
+
+  return (
+    <form
+      className="flex w-full max-w-sm flex-col rounded-lg border border-gray-300 p-4 dark:border-gray-700"
+      onSubmit={handleSubmit}
+    >
+      <h1 className="text-center text-xl font-bold">Log in</h1>
+      <Form.Label className="mt-4" htmlFor={usernameInputId}>
+        Username
+      </Form.Label>
+      <Form.TextInput
+        className="mt-2"
+        id={usernameInputId}
+        type="text"
+        {...form.register("username")}
+      />
+      {form.formState.errors.username ? (
+        <Form.Error className="mt-2">
+          {form.formState.errors.username.message}
+        </Form.Error>
+      ) : null}
+      <Form.Label className="mt-8" htmlFor={passwordInputId}>
+        Password
+      </Form.Label>
+      <Form.TextInput
+        className="mt-2"
+        id={passwordInputId}
+        type="password"
+        {...form.register("password")}
+      />
+      {form.formState.errors.password ? (
+        <Form.Error className="mt-2">
+          {form.formState.errors.password.message}
+        </Form.Error>
+      ) : null}
+      <Button className="mt-8" type="submit">
+        Log in
+      </Button>
+      {form.formState.errors.root ? (
+        <Form.Error className="mt-4 text-center">
+          {form.formState.errors.root.message}
+        </Form.Error>
+      ) : null}
+      <p className="mt-4 text-center">
+        Don{"'"}t have an account?{" "}
+        <Link
+          className="text-blue-700 hover:underline dark:text-blue-300"
+          href="/register"
+        >
+          Register here.
+        </Link>
+      </p>
+    </form>
+  );
+}
---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,36 @@
+import { db } from "@/prisma/client";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+export async function POST(request: NextRequest) {
+  const bodyParseResult = bodySchema.safeParse(await request.json());
+  if (!bodyParseResult.success) {
+    return NextResponse.json(null, { status: 400 });
+  }
+  const body = bodyParseResult.data;
+
+  const user = await db.user.findUnique({ where: { username: body.username } });
+  if (!user) {
+    return NextResponse.json(null, { status: 401 });
+  }
+  const passwordMatches = await argon2.verify(user.password, body.password);
+  if (!passwordMatches) {
+    return NextResponse.json(null, { status: 401 });
+  }
+
+  const response = new NextResponse();
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!);
+  response.cookies.set("token", token, {
+    httpOnly: true,
+    sameSite: true,
+    secure: true,
+  });
+  return response;
+}
