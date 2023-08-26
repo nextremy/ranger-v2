6ff05adc783efe9bfff1d46b4875100474b7b6c2
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-26 12:49:40 -0700

    Rename Prisma client variable

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -7,6 +7,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export const db = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV === "development") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "development") globalForPrisma.prisma = db;
