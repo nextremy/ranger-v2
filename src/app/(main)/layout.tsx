ba968c7f38e0c7c139b0c32ee71ef6dfba6f4f25
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-27 14:45:58 -0700

    Ensure popover out transition shows on navigation

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { Popover, Transition } from "@headlessui/react";
 import {
+  BellIcon,
   BellIcon as BellIconOutline,
+  Cog6ToothIcon,
   Cog6ToothIcon as Cog6ToothIconOutline,
   HomeIcon as HomeIconOutline,
   MagnifyingGlassIcon,
@@ -14,7 +17,7 @@ import {
 import { cx } from "class-variance-authority";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
+import { Fragment, ReactNode } from "react";
 
 export default function MainLayout(props: { children: ReactNode }) {
   const pathname = usePathname();
@@ -75,7 +78,66 @@ export default function MainLayout(props: { children: ReactNode }) {
           Settings
         </Link>
       </div>
-      <div className="grow">{props.children}</div>
+      <div className="grow">
+        <Popover>
+          {props.children}
+          <Transition
+            as={Fragment}
+            enter="transition-opacity ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Popover.Overlay className="fixed inset-0 bg-black/25" />
+          </Transition>
+          <Transition
+            as={Fragment}
+            enter="transition-transform ease-out duration-300"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition-transform ease-in duration-200"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <Popover.Panel className="fixed bottom-0 left-0 top-0 w-full max-w-xs border-r border-gray-300 bg-gray-100 p-2 dark:border-gray-700 dark:bg-gray-900">
+              <Popover.Button
+                as={Link}
+                className="flex h-14 items-center gap-4 rounded-md px-4 text-lg transition-colors duration-150 hover:bg-gray-200 hover:dark:bg-gray-800"
+                href="/home"
+              >
+                <HomeIconOutline className="h-6 w-6" />
+                Home
+              </Popover.Button>
+              <Popover.Button
+                as={Link}
+                className="flex h-14 items-center gap-4 rounded-md px-4 text-lg transition-colors duration-150 hover:bg-gray-200 hover:dark:bg-gray-800"
+                href="/search"
+              >
+                <MagnifyingGlassIcon className="h-6 w-6" />
+                Search
+              </Popover.Button>
+              <Popover.Button
+                as={Link}
+                className="flex h-14 items-center gap-4 rounded-md px-4 text-lg transition-colors duration-150 hover:bg-gray-200 hover:dark:bg-gray-800"
+                href="/notifications"
+              >
+                <BellIcon className="h-6 w-6" />
+                Notifications
+              </Popover.Button>
+              <Popover.Button
+                as={Link}
+                className="flex h-14 items-center gap-4 rounded-md px-4 text-lg transition-colors duration-150 hover:bg-gray-200 hover:dark:bg-gray-800"
+                href="/settings"
+              >
+                <Cog6ToothIcon className="h-6 w-6" />
+                Settings
+              </Popover.Button>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </div>
     </div>
   );
 }
