4cb91761a94add80743e66ca1099a8441d6938c1
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-28 15:48:56 -0700

    Tweak button styles to work with more elements

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,23 +2,26 @@ import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 import { ComponentPropsWithoutRef } from "react";
 
-const button = cva("rounded-md px-4 font-semibold disabled:opacity-50", {
-  variants: {
-    intent: {
-      primary:
-        "bg-blue-700 font-bold text-gray-100 duration-150 enabled:hover:bg-blue-800 dark:bg-blue-300 dark:text-gray-900 enabled:hover:dark:bg-blue-400",
+const button = cva(
+  "block flex items-center justify-center rounded-md px-4 font-semibold disabled:opacity-50",
+  {
+    variants: {
+      intent: {
+        primary:
+          "bg-blue-700 font-bold text-gray-100 duration-150 hover:bg-blue-800 disabled:hover:bg-blue-700 dark:bg-blue-300 dark:text-gray-900 hover:dark:bg-blue-400 disabled:hover:dark:bg-blue-300",
+      },
+      size: {
+        small: "h-12",
+        medium: "h-14",
+        large: "h-16",
+      },
     },
-    size: {
-      small: "h-12",
-      medium: "h-14",
-      large: "h-16",
+    defaultVariants: {
+      intent: "primary",
+      size: "small",
     },
   },
-  defaultVariants: {
-    intent: "primary",
-    size: "small",
-  },
-});
+);
 
 export function Button({
   asChild,
