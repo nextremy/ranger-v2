238dbed1730d9def940c702fce77a4ce5771431a
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-28 09:58:21 -0700

    Make top bar title bold

--- src/components/top-bar/title.tsx
+++ src/components/top-bar/title.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export function Title(props: { children: ReactNode }) {
-  return <h1 className="mx-2 text-lg font-medium">{props.children}</h1>;
+  return <h1 className="mx-2 text-lg font-bold">{props.children}</h1>;
 }
