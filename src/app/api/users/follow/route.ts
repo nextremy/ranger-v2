7ba8b2809c1bd8326759b52246664e8bde3c67ac
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-29 15:21:53 -0700

    Create follow/unfollow route handlers

---
+++ src/app/api/users/follow/route.ts
@@ -0,0 +1,26 @@
+import { db } from "@/prisma/client";
+import { getSession } from "@/utils/get-session";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  userId: z.string(),
+});
+
+export async function POST(request: NextRequest) {
+  const session = getSession();
+  if (!session) {
+    return NextResponse.json(null, { status: 401 });
+  }
+
+  const bodyParse = bodySchema.safeParse(await request.json());
+  if (!bodyParse.success) {
+    return NextResponse.json(null, { status: 400 });
+  }
+  const body = bodyParse.data;
+
+  await db.user.update({
+    data: { followers: { connect: { id: session.id } } },
+    where: { id: body.userId },
+  });
+}
